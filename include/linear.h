80e8fa315839aa97df71722a608e3ac6752d54b0
Aamir Ahmad <aamirahmadnagori9586@gmail.com>
2022-06-09 16:43:23 +0530

    Add Linear Sort Generic Part

---
+++ include/linear.h
@@ -0,0 +1,4 @@
+#ifndef __$__LINEAR_SORT_H
+#define __$__LINEAR_SORT_H 12345
+void linearSort(void *,int,int,int (*)(void*,void*));
+#endif
---
+++ src/linear.c
@@ -0,0 +1,30 @@
+#ifndef __$__LINEAR_SORT_C
+#define __$__LINEAR_SORT_C 12345
+#include<stdlib.h>
+#include<string.h>
+#include<linear.h>
+void linearSort(void *ptr,int collectionSize,int elementSize,int (*pointerToFunction)(void*,void*))
+{
+void *a,*b,*c;
+c=(void*)malloc(collectionSize);
+int e,f;
+e=0;
+while(e<=(collectionSize-2))
+{
+f=e+1;
+while(f<=(collectionSize-1))
+{
+a=ptr+(e*elementSize);
+b=ptr+(f*elementSize);
+if(pointerToFunction(b,a)<0)
+{
+memcpy(c,a,elementSize);
+memcpy(a,b,elementSize);
+memcpy(b,c,elementSize);
+}
+f++;
+}
+e++;
+}
+}
+#endif
